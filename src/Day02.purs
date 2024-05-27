e87bf3aa9cd59d244198908c0d61858fb3807f7e
Alexander Davidson Bryan <alxdb@pm.me>
2024-05-27 13:17:05 +0100

    day2 (part1)

--- src/Day02.purs
+++ src/Day02.purs
@@ -1,7 +1,64 @@
 module Day02 (day02) where
 
-import Data.Maybe (Maybe(..))
+import Prelude
+
+import Debug (spy)
+
+import Control.Alt ((<|>))
+import Data.Either (hush)
+import Data.Foldable (sum, foldl, find)
+import Data.List (List)
+import Data.Maybe (Maybe, isNothing)
+import Data.String.Utils (lines, trimEnd)
+import Data.Traversable (traverse)
+import Data.Tuple (Tuple(..))
+import Parsing (Parser, runParser)
+import Parsing.Combinators (sepBy)
+import Parsing.String (string)
+import Parsing.String.Basic (intDecimal, space)
+
+type Hand = { r :: Int, g :: Int, b :: Int }
+type Game = { id :: Int, hands :: List Hand }
+data Cube = R | G | B
 
 day02 :: String -> Maybe Int
-day02 _ = Nothing
+day02 input = sum <$> (trimEnd >>> lines >>> traverse go) input
+  where
+  go line = do
+    game <- parseGame (spy "line" line)
+    pure $ if (spy "isPossible" $ isGamePossible game) then game.id else 0
+
+isGamePossible :: Game -> Boolean
+isGamePossible game = isNothing $ find isHandImpossible (spy "hands" game.hands)
+  where
+  isHandImpossible { r, g, b } = (r > 12) || (g > 13) || (b > 14)
+
+parseGame :: String -> Maybe Game
+parseGame line = hush $ runParser line gameParser
+
+gameParser :: Parser String Game
+gameParser = do
+  id <- string "Game " *> intDecimal
+  hands <- string ": " *> handParser `sepBy` string "; "
+  pure { id: id, hands: hands }
+
+handParser :: Parser String Hand
+handParser = do
+  cubes <- cubeParser `sepBy` string ", "
+  pure $ foldl f { r: 0, g: 0, b: 0 } cubes
+  where
+  f counts (Tuple cube count) = case cube of
+    R -> counts { r = count }
+    G -> counts { g = count }
+    B -> counts { b = count }
+
+cubeParser :: Parser String (Tuple Cube Int)
+cubeParser = do
+  count <- intDecimal
+  cube <- space *>
+    ( string "red" *> pure R
+        <|> string "green" *> pure G
+        <|> string "blue" *> pure B
+    )
+  pure $ Tuple cube count
 
--- test/Day02Spec.purs
+++ test/Day02Spec.purs
@@ -2,12 +2,22 @@ module Test.Day02Spec where
 
 import Prelude
 
+import Day02 (day02)
+
 import Test.Spec (Spec, it, describe)
 import Test.Spec.Assertions (shouldEqual)
 
 spec :: Spec Unit
 spec = describe "Day02" do
   it "handles the default example" do
-    let result = "foo"
-    let expect = "foo"
+    let
+      example =
+        """Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
+"""
+    let result = day02 example
+    let expect = pure 8
     result `shouldEqual` expect
