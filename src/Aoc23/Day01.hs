9e207b1def751a8c8fa641adb7d09baf8f19eaf2
Alexander Davidson Bryan <alxdb@pm.me>
2024-06-13 23:32:38 +0100

    add parser suffixes

--- src/Aoc23/Day01.hs
+++ src/Aoc23/Day01.hs
@@ -15,28 +15,28 @@ solution :: Solution
 solution = sumLines extractCalibrationValue
 
 extractCalibrationValue :: String -> Either String Int
-extractCalibrationValue = fmapL errorMessage . parse calibrationValue
+extractCalibrationValue = fmapL errorMessage . parse calibrationValueParser
  where
   errorMessage [Empty] = "Couldn't find a digit in input line"
   errorMessage _ = undefined
 
-calibrationValue :: Parser Char Int
-calibrationValue = do
-  ds <- some . next $ digit
+calibrationValueParser :: Parser Char Int
+calibrationValueParser = do
+  ds <- some . next $ digitParser
   return $ head ds * 10 + last ds
 
-digit :: Parser Char Int
-digit =
+digitParser :: Parser Char Int
+digitParser =
   digitToInt
     <$> satisfy isDigit
     <|> do
       -- Parses portmenteaus of digits
-      s <- lookAhead spelledDigit
+      s <- lookAhead spelledDigitParser
       _ <- anything
       return s
 
-spelledDigit :: Parser Char Int
-spelledDigit =
+spelledDigitParser :: Parser Char Int
+spelledDigitParser =
   asum . map (\(s, x) -> exact s $> x) $
     [ ("one", 1)
     , ("two", 2)
