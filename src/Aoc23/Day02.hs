e35f05924f1143d1d973ed4db2741e6157566fb1
Alexander Davidson Bryan <alxdb@pm.me>
2024-06-18 09:59:56 +0100

    day2 part 2

--- src/Aoc23/Day02.hs
+++ src/Aoc23/Day02.hs
@@ -1,4 +1,4 @@
-module Aoc23.Day02 (solution_1) where
+module Aoc23.Day02 (solution_1, solution_2) where
 
 import Prelude hiding (id)
 
@@ -11,13 +11,16 @@ import Data.Bifunctor (Bifunctor (bimap))
 solution_1 :: Solution
 solution_1 = sumLines lineSolution_1
 
-solution_1_hand :: Hand
-solution_1_hand = Hand{red = 12, green = 13, blue = 14}
+solution_2 :: Solution
+solution_2 = sumLines lineSolution_2
 
 lineSolution_1 :: String -> Either String Int
 lineSolution_1 = bimap show go . parse gameParser
  where
-  go game = if gameIsPossible solution_1_hand game then game.id else 0
+  go game = if gameIsPossible (Hand 12 13 14) game then game.id else 0
+
+lineSolution_2 :: String -> Either String Int
+lineSolution_2 = bimap show (handPower . minimumPossibleCubes) . parse gameParser
 
 data Game = Game {id :: Int, hands :: [Hand]}
 data Hand = Hand {red :: Int, green :: Int, blue :: Int}
@@ -27,6 +30,14 @@ gameIsPossible :: Hand -> Game -> Bool
 gameIsPossible refHand Game{hands} = all handIsPossible hands
  where
   handIsPossible Hand{red, green, blue} = red <= refHand.red && green <= refHand.green && blue <= refHand.blue
+
+minimumPossibleCubes :: Game -> Hand
+minimumPossibleCubes Game{hands} = foldl go (Hand 0 0 0) hands
+ where
+  go acc hand = Hand (max acc.red hand.red) (max acc.green hand.green) (max acc.blue hand.blue)
+
+handPower :: Hand -> Int
+handPower (Hand r g b) = r * g * b
 
 gameParser :: Parser Char Game
 gameParser = do
--- test/Aoc23/Day02Spec.hs
+++ test/Aoc23/Day02Spec.hs
@@ -7,8 +7,8 @@ import Aoc23.Solution
 
 import Test.Hspec
 
-example_1 :: String
-example_1 =
+exampleGames :: String
+exampleGames =
   "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green\n\
   \Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue\n\
   \Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red\n\
@@ -18,6 +18,9 @@ example_1 =
 spec :: Spec
 spec = describe "Aoc23.Day02" $ do
   describe "solution" $ do
-    it "solves the first sample solution" $
-      runSolution solution_1 example_1
+    it "solves the first problem" $
+      runSolution solution_1 exampleGames
         `shouldBe` Right 8
+    it "solves the second problem" $
+      runSolution solution_2 exampleGames
+        `shouldBe` Right 2286
