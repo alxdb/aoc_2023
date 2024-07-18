89ca4d1d6b0d7de0adbcc88c26b941948668e821
alxdb <alxdb@pm.me>
2024-07-18 20:19:41 +0100

    improve performance

--- src/Aoc23/Day04.hs
+++ src/Aoc23/Day04.hs
@@ -1,10 +1,12 @@
 module Aoc23.Day04 (solution_1, solution_2) where
 
 import Aoc23.Solution
+import Control.Monad.Extra
+import Control.Monad.State
 import Core.Parser.Char
 import Core.Parser.Combinator
-import Data.List
-import Data.Vector (Vector)
+import Data.IntMap hiding (foldl)
+import Data.List hiding (insert)
 import Data.Vector qualified as V
 import Flow
 import Prelude
@@ -15,10 +17,16 @@ solution_1 = sumLinesParser cardParser <| getPoints <. matches
   getPoints x = if x < 1 then 0 else 2 ^ (x - 1)
 
 solution_2 :: Solution
-solution_2 = parserSolution (lineParser cardParser) <| go <. V.fromList
+solution_2 = parserSolution (lineParser cardParser) <| totalCardsWon <. V.fromList
  where
-  go cards =
-    let allWonCards = concatMap (winningCards cards) cards
+  totalCardsWon cards =
+    let allWonCards = evalState (concatMapM getWonCards (V.toList cards)) empty
+        getWonCards card = gets (!? card.cardId) >>= maybe findCardWins return
+         where
+          findCardWins = do
+            let thisCardWins = [cards V.! (card.cardId + i - 1) | i <- [1 .. matches card]]
+            subCardwins <- (++ thisCardWins) <$> concatMapM getWonCards thisCardWins
+            state (\cache -> (subCardwins, insert card.cardId subCardwins cache))
      in length allWonCards + length cards
 
 data Card = Card
@@ -37,8 +45,3 @@ cardParser = do
 
 matches :: Card -> Int
 matches Card {wins, have} = length $ intersect wins have
-
-winningCards :: Vector Card -> Card -> [Card]
-winningCards cards card@Card {cardId} =
-  let thisCardWins = [cards V.! (cardId + i - 1) | i <- [1 .. matches card]]
-   in thisCardWins ++ concatMap (winningCards cards) thisCardWins
