cd82ee21bfccccf10697bcd444506d92881f29b6
alxdb <alxdb@pm.me>
2024-07-26 18:36:04 +0100

    day5 start

--- src/Aoc23/Day05.hs
+++ src/Aoc23/Day05.hs
@@ -1,8 +1,19 @@
 module Aoc23.Day05 (solution_1) where
 
 import Prelude
+import Flow
 
 import Aoc23.Solution
 
+import Core.Parser.Char
+
 solution_1 :: Solution
-solution_1 = Solution $ \_ -> Right 0
+solution_1 = parserSolution almanacParser (min <. findLocation)
+
+newtype Almanac = Almanac {seeds :: [Int]} deriving (Show, Eq)
+
+almanacParser :: ParserC Almanac
+almanacParser = return $ Almanac [1,2,3]
+
+findLocation :: Almanac -> [Int]
+findLocation _ = [1,2,3]
